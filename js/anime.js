a82fd384dbc6f33a1afd61012f54832e63a20fca
Kfnn12 <132144405+Kfnn12@users.noreply.github.com>
2023-12-05 11:25:00 +0800

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,7 +1,7 @@
 // Api urls
 
 const infoapi = "https://animxer-api-cvxg.vercel.app/anime/gogoanime/info/";
-const epapi = "http://api.anime-dex.workers.dev/anime/gogoanime/episodes/";
+const epapi = "https://animxer-api-cvxg.vercel.app/anime/gogoanime/episodes/";
 const searchapi = "https://animxer-api-cvxg.vercel.app/meta/anilist/";
 const anilistinfoapi = "https://animxer-api-cvxg.vercel.app/meta/anilist/info/";
 const gogosearchapi = "https://animxer-api-cvxg.vercel.app/anime/gogoanime/";
